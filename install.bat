369fa2dae2c32eec2b9d2ef59b26d59b0d88f08d
tye2vaild <2yehill10@gmail.com>
2026-02-02 12:32:21 -0600

    Create install.bat for Windows installation script

---
+++ install.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Windows installation script for NFL Football Game
+
+REM Create a directory for the game
+mkdir "NFL_Football_Game"
+
+REM Copy game files from source (assuming they are stored in a specific location)
+:: Copy command would go here
+
+REM Install dependencies (if any)
+:: Install command would go here
+
+echo Installation complete!
